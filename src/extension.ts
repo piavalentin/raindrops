34af0dd3c1e023081a239bda081a5dc075df99d7
Pia Valentin <9430235+piavalentin@users.noreply.github.com>
2024-06-15 01:52:49 +0200

    chore: housekeeping

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,13 @@
 import * as vscode from "vscode";
 
 function getNonce() {
-	let text = '';
-	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < 32; i++) {
-		text += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return text;
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
 
 export class ColorsViewProvider implements vscode.WebviewViewProvider {
